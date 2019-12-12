ca34c2cc89eff2e0cdc773c5899677ef2eeedb22
evttenorio <evttenorio@gmail.com>
2019-12-12 18:11:30 -0300

    up project

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask import request, redirect, url_for, render_template
+#from flask.ext.security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:senha@localhost/vpslab'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mysecretpassword@157.245.243.14:5432/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -20,8 +22,21 @@ class User(db.Model):
 
 @app.route('/')
 def index():
-    return "<h1>O Flask</h1>"
+    myUser = User.query.all()
+    #oneItem = User.query.filter_by(username='test2').all()
+    return render_template('add_user.html', myUser=myUser)
 
+@app.route('/profile/<username>')
+def profile(username):
+    user = User.query.filter_by(username=username).first()
+    return render_template('profile.html', user=user)
+
+@app.route('/post_user', methods=['POST'])
+def post_user():
+    user = User(request.form['username'], request.form['email'])
+    db.session.add(user)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__=="__main__":
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
