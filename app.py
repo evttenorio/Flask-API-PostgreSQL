575e95d9bc9cd892a3c07c78540ee21b24244097
evttenorio <evttenorio@gmail.com>
2019-12-13 01:41:22 +0000

    Up project

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask import request, redirect, url_for, render_template
-#from flask.ext.security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
+#from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:l4bs020192@banco/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'super-secret'
+app.config['SECURITY_REGISTERABLE'] = True
 db = SQLAlchemy(app)
 
 class User(db.Model):
@@ -20,16 +22,18 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+
 @app.route('/')
 def index():
-    myUser = User.query.all()
-    #oneItem = User.query.filter_by(username='test2').all()
-    return render_template('add_user.html', myUser=myUser)
+    #oneItem = User.query.filter_by(email='test2').all()
+    return render_template('add_user.html')
 
-@app.route('/profile/<username>')
-def profile(username):
-    user = User.query.filter_by(username=username).first()
-    return render_template('profile.html', user=user)
+@app.route('/cadastrados')
+def cadastrados():
+    user = User.query.all()
+    myUser = User.query.all()
+    oneItem = User.query.filter_by(username='Maria').all() 
+    return render_template('profile.html', user=user, myUser=myUser)
 
 @app.route('/post_user', methods=['POST'])
 def post_user():
